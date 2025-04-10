f41a5c442ea544fd5a30dc468db244fcfd43805f
Stephan Fouire <24784001@sun.ac.za>
2025-04-10 11:34:18 +0200

    voip server and client

---
+++ VoIPServer.java
@@ -0,0 +1,53 @@
+import javax.sound.sampled.*;
+import java.net.*;
+
+public class VoIPServer {
+    public static void main(String[] args) throws Exception {
+        int port = 50005;
+        DatagramSocket socket = new DatagramSocket(port);
+
+        AudioFormat format = new AudioFormat(44100.0f, 16, 1, true, false);
+        DataLine.Info infoOut = new DataLine.Info(SourceDataLine.class, format);
+        SourceDataLine speaker = (SourceDataLine) AudioSystem.getLine(infoOut);
+        speaker.open(format);
+        speaker.start();
+
+        DataLine.Info infoIn = new DataLine.Info(TargetDataLine.class, format);
+        TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(infoIn);
+        microphone.open(format);
+        microphone.start();
+
+        InetAddress clientAddress = InetAddress.getByName("100.69.42.0"); // <-- replace with actual client IP
+        int clientPort = 50006;
+
+        Thread receiveThread = new Thread(() -> {
+            try {
+                byte[] buffer = new byte[4096];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                while (true) {
+                    socket.receive(packet);
+                    speaker.write(packet.getData(), 0, packet.getLength());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread sendThread = new Thread(() -> {
+            try {
+                byte[] buffer = new byte[4096];
+                while (true) {
+                    microphone.read(buffer, 0, buffer.length);
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
+                    socket.send(packet);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        receiveThread.start();
+        sendThread.start();
+    }
+}
+
