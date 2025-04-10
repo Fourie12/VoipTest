c90aa18022b8506f5eac6138c999e9424c7dbbee
Stephan Fouire <24784001@sun.ac.za>
2025-04-10 16:52:19 +0200

    who you gonna call?

--- server/call.java
+++ server/call.java
@@ -1,9 +1,14 @@
 package server;
 
 import server.Voip;
+import java.util.Scanner;
 
 public class call {
 	public static void main(String[] args) {
-		Voip voip = new Voip("100.69.42.0");
+		Scanner scanner = new Scanner(System.in);
+		String ip;
+		System.out.println("Who you gonna call?");
+		ip = scanner.nextLine();
+		Voip voip = new Voip(ip);
 	}
 }
